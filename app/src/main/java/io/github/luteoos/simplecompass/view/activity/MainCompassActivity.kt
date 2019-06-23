77a7e2f29c24045033099fb46151cd10461966eb
Luteoos <lmati97@gmail.com>
2019-06-23 14:21:19 +0200

    added logging, fixed style

--- app/src/main/java/io/github/luteoos/simplecompass/view/activity/MainCompassActivity.kt
+++ app/src/main/java/io/github/luteoos/simplecompass/view/activity/MainCompassActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import com.luteoos.kotlin.mvvmbaselib.BaseActivityMVVM
 import io.github.luteoos.simplecompass.R
 import io.github.luteoos.simplecompass.viewmodel.CompassViewModel
+import timber.log.Timber
 
 class MainCompassActivity : BaseActivityMVVM<CompassViewModel>() {
 
@@ -11,6 +12,7 @@ class MainCompassActivity : BaseActivityMVVM<CompassViewModel>() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Timber.i("init ${this}")
         viewModel = getViewModel(this)
     }
 }
--- app/src/main/java/io/github/luteoos/simplecompass/viewmodel/CompassViewModel.kt
+++ app/src/main/java/io/github/luteoos/simplecompass/viewmodel/CompassViewModel.kt
@@ -1,6 +1,10 @@
 package io.github.luteoos.simplecompass.viewmodel
 
 import com.luteoos.kotlin.mvvmbaselib.BaseViewModel
+import timber.log.Timber
 
 class CompassViewModel : BaseViewModel() {
+    init {
+        Timber.i("init ${this}")
+    }
 }
